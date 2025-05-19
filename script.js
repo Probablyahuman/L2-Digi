ba4b5ca83edee7f591f9558e14adee5aea674d45
Probablyahuman <delavan.mackey@inglewoodhs.school.nz>
2025-05-20 10:50:48 +1200

    Work on modal

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 console.log("js launched");
 
+var modal = document.getElementById("ans-feedback-modal");
+
 function modalDisplay(newState) {
   if (newState) {
     document.getElementById("ans-feedback-modal").style.display = "block";
   } else {
-    document.getElementById("ans-feedback-modal").style.display = "none";
+    modal.style.display = "none";
   }
   
   /*alert('Button clicked!');*/
 }
+/*
+window.onclick() = function(event) {
+  if event.target == document.getElement
+}
+*/
