ca01c81ad3c6bd089314988ba1069ff86870dcee
Probablyahuman <delavan.mackey@inglewoodhs.school.nz>
2025-05-20 10:09:54 +1200

    Modal skeleton made

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ console.log("js launched");
 
 function modalDisplay(newState) {
   if (newState) {
-  document.getElementById("ans-feedback-modal").style.display = "block";
+    document.getElementById("ans-feedback-modal").style.display = "block";
+  } else {
+    document.getElementById("ans-feedback-modal").style.display = "none";
   }
   
-  alert('Button clicked!');
+  /*alert('Button clicked!');*/
 }
