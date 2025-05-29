a0accfe4ca616b57ead1dfac8a38df4df5b43dca
Probablyahuman <delavan.mackey@inglewoodhs.school.nz>
2025-05-29 15:12:29 +1200

    Added access keys, still need to explain them to user

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let modal; /*A variable to handle modals, either 0 for no modal, or the id of th
 
 function modalDisplay(newState) {
   if (newState) {
-    if (!modal) {
+    if (modal) {
+      modal.style.display = "none";
+    }
+    if (newState != modal) {
       modal = document.getElementById(newState);
       modal.style.display = "block";
-      }
+    }
   } else {
     modal.style.display = "none";
     modal = 0;
@@ -18,5 +21,16 @@ function modalDisplay(newState) {
 window.onclick = function(event) {
   if (event.target == modal) {
     modalDisplay(0);
+  }
+};
+
+document.onkeydown = function(key) {
+  if (key.key == "Enter") {
+    console.log("Enter Pressed");
+    if (modal) {
+      modalDisplay(0);
+    } else {
+      alert("hello");
+    }
   }
 };
