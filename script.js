0b93e30628e38c6d4b670a612789922038d59654
Probablyahuman <delavan.mackey@inglewoodhs.school.nz>
2025-05-30 13:27:52 +1200

    Enter behaviour improved

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 console.log("js launched");
 let modal; /*A variable to handle modals, either 0 for no modal, or the id of the current modal*/
+let lastKey; /*Stores the last key pressed, used for managing enter behaviour*/
 
 function modalDisplay(newState) {
+  document.activeElement.blur();
+  /*Remove tab focus whenever modal is opened or closed,
+    prevents buggy behaviour like content behind modal remaining focused*/
   if (newState) {
+    /*If a modal has been called to open otherwise this returns false and closes the modal */
     if (modal) {
       modal.style.display = "none";
     }
@@ -24,13 +29,17 @@ window.onclick = function(event) {
   }
 };
 
-document.onkeydown = function(key) {
-  if (key.key == "Enter") {
+document.addEventListener("keydown", function(e) {
+  
+  if (e.key == "Enter" & lastKey != 'Tab') {
     console.log("Enter Pressed");
     if (modal) {
       modalDisplay(0);
+      e.preventDefault();
     } else {
       alert("hello");
     }
   }
-};
+  
+  lastKey = e.key; /* Always record the most recent key pressed */
+});
