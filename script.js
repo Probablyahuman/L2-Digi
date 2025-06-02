70236d21fe1066c2cf24d76a3faf459d149484d2
Probablyahuman <delavan.mackey@inglewoodhs.school.nz>
2025-06-03 11:08:20 +1200

    Js event handlers improved

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 console.log("js launched");
 let modal; /*A variable to handle modals, either 0 for no modal, or the id of the current modal*/
 let lastKey; /*Stores the last key pressed, used for managing enter behaviour*/
+let ansInput = document.getElementById("ans-input");
+console.log(ansInput);
 
 function modalDisplay(newState) {
+  /*Updates the modal with the input, newState is either ID of new modal or a False value to close all modals */
   document.activeElement.blur();
   /*Remove tab focus whenever modal is opened or closed,
     prevents buggy behaviour like content behind modal remaining focused*/
-  if (newState) {
-    /*If a modal has been called to open otherwise this returns false and closes the modal */
-    if (modal) {
-      modal.style.display = "none";
-    }
-    if (newState != modal) {
+  if (newState && document.getElementById(newState) != modal) {
+    /*If a new modal has been called to open , otherwise this returns false and closes the modal */
+      if (modal) {
+      /*If there is already a modal, close it before opening the new one */
+      modalDisplay(0);
+      }
       modal = document.getElementById(newState);
       modal.style.display = "block";
-    }
   } else {
     modal.style.display = "none";
     modal = 0;
+    ansInput.focus();
   }
-  
-  /*alert('Button clicked!');*/
 }
 
-window.onclick = function(event) {
+document.addEventListener("click", function(e) {
+  /*Close the current modal when clicking on the greyed background of it */
   if (event.target == modal) {
     modalDisplay(0);
   }
-};
+});
 
-document.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function(e) {
+  /*The general event listener for specially bound keys */
+  /*console.log(e.key); */
   
-  if (e.key == "Enter" & lastKey != 'Tab') {
-    console.log("Enter Pressed");
-    if (modal) {
-      modalDisplay(0);
-      e.preventDefault();
-    } else {
-      alert("hello");
+  switch (e.key) {
+    case "Enter":
+      if (lastKey != 'Tab') {
+        if (modal) {
+          modalDisplay(0);
+          e.preventDefault();
+        } else {
+        alert("hello");
+        }
+      }
+      break;
+
+    case "Escape":
+      if (modal) {
+        modalDisplay(0);
+      } else {
+        ansInput.focus();
+      }
+    break;
+      
     }
-  }
-  
   lastKey = e.key; /* Always record the most recent key pressed */
 });
